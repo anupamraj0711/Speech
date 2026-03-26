6d455ee5a847707dca9009ba8f16cc6d7a60d41f
Anupam Raj <anupam99nic@gmail.com>
2026-03-27 04:17:15 +0530

    update

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
 let speech = new SpeechSynthesisUtterance();
 
+
+//add voices to the select element
+//voice change event
+let voices = [];
+
+let voiceSelect = document.querySelector("select");
+
+window.speechSynthesis.onvoiceschanged = () => {
+    voices = window.speechSynthesis.getVoices();
+    
+    speech.voice = voices[0];
+
+    //voiceSelect.innerHTML = ""; // clear previous options
+
+    voices.forEach((voice, i) => (voiceSelect.options[i] = new Option(voice.name, i)));
+};
+
+// Change voice when user selects
+voiceSelect.addEventListener("change", () => {
+    speech.voice = voices[voiceSelect.value];
+});
+
 document.querySelector("button").addEventListener("click", () => {
     speech.text = document.querySelector("textarea").value;
     window.speechSynthesis.speak(speech);
